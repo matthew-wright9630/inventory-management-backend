fc826db8aa58dde967c43a417eee7bac9710ca52
Matthew Wright <matthew.wright9630@gmail.com>
2025-12-10 14:58:32 -0500

    Update index.html and script.js

---
+++ inventory-management-frontend/script.js
@@ -0,0 +1,8 @@
+const URL = "http://localhost:8080";
+let warehouses = [];
+
+function addWarehouseToList(newWarehouse) {
+    let titleEl = document.createElement("h2");
+    let addressEl = document.createElement("p");
+    let capacityEl = document.createElement("p");
+}
